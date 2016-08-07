32d19788dc0dac51b4a28fe9e1f4586a737b2df1
王西迪 <wangxidi@wangxidideMacBook.local>
2016-08-07 16:15:50 +0800

    add option to cmake ,option for  complite the exampe

--- examples/ini_dump.c
+++ examples/ini_dump.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 #include <string.h>
-#include "../ini.h"
+#include "ini.h"
 
 static int dumper(void* user, const char* section, const char* name,
                   const char* value)
